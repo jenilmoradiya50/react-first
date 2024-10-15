6aea37d74f325c7ec0fba1d1e7d5c3f77cc613c4
Jenil Moradiya <jenilmoradiya816@gmail.com>
2024-10-15 11:37:13 +0530

    added new project

--- 01basicsvite/src/main.jsx
+++ 01basicsvite/src/main.jsx
@@ -1,9 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
- 
-    <App />
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  childen: "Click me to visit google",
+};
 
-)
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom React app</h1>
+    </div>
+  );
+}
+const AnotherElement = (
+  <a href="http://google.com" target="_blank">
+    Visit Google
+  </a>
+);
+
+const areactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "click to visit google"
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
